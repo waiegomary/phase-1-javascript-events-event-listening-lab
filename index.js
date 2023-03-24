c32ba79ca94f68e5846c598716ea532f8e198d5e
mary_waiyego <waiyegomary55@gmail.com>
2023-03-24 14:29:11 +0300

    forms

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-
-const input = document.getElementById('input');
-
-function clickAlert() {
-    alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
-
-
+<form id="create-task-form" action="#" method="POST">
+    <label for="new-task-description">Task description:</label>
+    <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+        <input type="submit" value="Create New Task">
+        </form>
